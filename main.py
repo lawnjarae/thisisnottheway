2ecdbecd34dd7ee21c28255b7c05a9e79a7bfedf
Justin Jarae <lawnjarae@gmail.com>
2022-08-21 21:37:37 -0600

    Use list; hot instead of top;

--- main.py
+++ main.py
@@ -12,24 +12,111 @@ reddit = praw.Reddit(
     username=os.environ.get("REDDIT_USERNAME")
 )
 
+list_of_subs = [
+  'all',
+  '321',
+  'AdrenalinePorn',
+  'Android',
+  'aquaponics',
+  'ArtisanVideos',
+  'Backcountry',
+  'baseball',
+  'basejumping',
+  'bicycletouring',
+  'BlackLivesMatter',
+  'CatsAreAssholes',
+  'CFB',
+  'COclimbing',
+  'Colorado',
+  'ColoradoRockies',
+  'Coronavirus',
+  'COsnow',
+  'dadjokes',
+  'darknetdiaries',
+  'Denver',
+  'denvermusic',
+  'DenverProtests',
+  'DIY',
+  'energy',
+  'environment',
+  'florida',
+  'freeflight',
+  'freefolk',
+  'fsusports',
+  'Fullmoviesonvimeo',
+  'funny',
+  'geek',
+  'gifs',
+  'HistoryPorn',
+  'hockey',
+  'InfowarriorRides',
+  'MTB',
+  'Music',
+  'newretrowave',
+  'ODroid',
+  'oldfreefolk',
+  'OnePunchMan',
+  'OpenArgs',
+  'overlanding',
+  'pics',
+  'politics',
+  'programming',
+  'RenewableEnergy',
+  'restofthefuckingowl',
+  'sailing',
+  'science',
+  'SkyDiving',
+  'snowboarding',
+  'Spliddit',
+  'Stargate',
+  'StarWarsleftymemes',
+  'TampaBayLightning',
+  'tampabayrays',
+  'TeardropTrailers',
+  'technology',
+  'THE_PACK',
+  'TheDollop',
+  'TheFulmerCup',
+  'TinyHouses',
+  'todayilearned',
+  'TrailGuides',
+  'TunnelFlight',
+  'vandwellers',
+  'videos',
+  'woodworking',
+  'worldcup',
+  'worldnews',
+  'WTF',
+]
+
 # Get a list of subs for the bot. This could also be done just with an array.
 # The array method is probably the best. That way you can just modify the code.
-subbed_to = list(reddit.user.subreddits(limit=None))
+# list_of_subs = list(reddit.user.subreddits(limit=None))
+
+num_downvotes = 0
 
 # Loop dem subs
-for subreddit in subbed_to:
-  print(f'Scanning {subreddit.display_name_prefixed}')
+for sub in list_of_subs:
+  subreddit = reddit.subreddit(sub)
+  print(f'Scanning r/{subreddit.display_name}')
   # Get the n top submissions from that sub
-  hot_subs = subreddit.top(time_filter='day', limit=15)
-  for submission in hot_subs:
+  if sub != 'all':
+    limit = 15
+  else:
+    limit = 50
+  # top_subs = subreddit.top(time_filter='day', limit=limit)
+  hot_subsmissions = subreddit.hot(limit=limit)
+  for submission in hot_subsmissions:
     # https://praw.readthedocs.io/en/stable/tutorials/comments.html#the-replace-more-method
     submission.comments.replace_more(limit=7)
     all_comments = submission.comments.list()
-    print(f'Submission: {submission.title} has {len(all_comments)} comments.')
+    print(f'{submission.subreddit_name_prefixed}: {submission.title} has {len(all_comments)} comments.')
     
     # Check if this comment starts with "this is the way"
     for comment in all_comments:
       if comment.body.lower().startswith('this is the way'):
-        print('WE GOT ONE!!!') # Ghostbusters
-        print(comment)
+        print(f'WE GOT ONE!!! {comment.author.name} is a clown.') # Ghostbusters
         comment.downvote()
+        num_downvotes = num_downvotes + 1
+
+print(f'Downvoted {num_downvotes} comments for being useless.')
